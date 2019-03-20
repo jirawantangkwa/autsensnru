d452ee388bd7ce3917ab364db84eb2f3895b0015
Jirawan <student21@Student21.local>
2019-03-20 14:08:57 +0700

    Get Event From Click Success

--- AuthenSSRU/ViewController.swift
+++ AuthenSSRU/ViewController.swift
@@ -6,17 +6,29 @@
 //  Copyright © 2562 Jirawan. All rights reserved.
 //
 
+
+
+
 import UIKit
 
-
-
 class ViewController: UIViewController {
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    } //Main Methhod
+
+   
+    @IBAction func registerButton(_ sender: UIButton) {
+            print("You Click Register")
+        
+        
     }
 
-
-}
+    
+    
+} //Main Class
 
